ec05c1793acfc3aa5bb582459af9c812c44587f2
Anđela Bralović <bralovic.andj@gmail.com>
2022-09-15 11:57:08 +0200

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+const menuBtn = document.querySelector('.menu-btn')
+const navLinks = document.querySelector('.navbar')
+
+$(document).ready(function(){
+  $("#prvi").click(function(){
+    
+  });
+}); 
+
+
+
