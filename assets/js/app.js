f8f6eb553661e8cb1e5a84e7b7bb714e9ba31618
akibralovic <bralovic.andj@gmail.com>
2022-10-26 16:32:32 +0200

    mob-menu
    Signed-off-by: akibralovic <bralovic.andj@gmail.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,14 @@
+
 window.addEventListener('DOMContentLoaded', (event) => {
    //inicijalizacija
  });
+
+ function mobileMenuFunction (){
+    const x = document.getElementById("mobile-menu");
+    if(x.style.display === "block"){
+      x.style.display = "none";
+    }
+    else{
+      x.style.display = "block";
+    }
+ }
