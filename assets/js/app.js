19cd8e72136a3a97a4ac54c16b0ebd0516d60271
akibralovic <bralovic.andj@gmail.com>
2022-11-18 13:52:58 +0100

    validation
    Signed-off-by: akibralovic <bralovic.andj@gmail.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,125 +1,154 @@
 const phoneMenu = () => {
   //pristupamo dom elementima sa kojima radimo
-  const mobMenu = document.getElementById('burger-menu');
-  const phoneDiv = document.getElementById('mobile-menu');
+  const mobMenu = document.getElementById("burger-menu");
+  const phoneDiv = document.getElementById("mobile-menu");
 
   //ovdo je ekvivalent stejta u reactu, prati da li nesto zelimo da prikazemo ili ne
   let active = true;
-  console.log('radi')
-  mobMenu.addEventListener('click', () => {
+  console.log("radi");
+  mobMenu.addEventListener("click", () => {
     //ako je active nije true, dodajemo mu klasu iz css-a, koja sakriva element
     if (!active) {
       //classList.add dodaje css klasu
-      phoneDiv.classList.add('d-none')
+      phoneDiv.classList.add("d-none");
       //vracamo da bude true, kako bi se kod sledeceg klika bilo true i ona prikazalo element
-      return active = true;
+      return (active = true);
     }
     //ako je active true, mi zelimo da se ukloni klasa iz css-a koja sakriva element
     if (active) {
       //classList.remove uklanja css klasu
-      phoneDiv.classList.remove('d-none')
+      phoneDiv.classList.remove("d-none");
       //na kraju vracamo da active bude false
-      return active = false;
+      return (active = false);
     }
-  })
-
-}
+  });
+};
 
 const catMenu = () => {
-  const catMenu = document.getElementById('cat-menu');
-  const catDiv = document.getElementById('cat-mobile');
+  const catMenu = document.getElementById("cat-menu");
+  const catDiv = document.getElementById("cat-mobile");
 
   let active = true;
-  console.log('radi');
+  console.log("radi");
 
-  catMenu.addEventListener('click', () => {
+  catMenu.addEventListener("click", () => {
     if (!active) {
-      catDiv.classList.add('d-none')
+      catDiv.classList.add("d-none");
 
-      return active = true;
+      return (active = true);
     }
     if (active) {
-      catDiv.classList.remove('d-none')
+      catDiv.classList.remove("d-none");
 
-      return active = false;
+      return (active = false);
     }
-
-  })
-}
+  });
+};
 
 const searchMenu = () => {
-  const srchMenu = document.getElementById('mob-srch');
-  const srchDiv = document.getElementById('search-mobile');
+  const srchMenu = document.getElementById("mob-srch");
+  const srchDiv = document.getElementById("search-mobile");
 
-  const logo = document.getElementById('logo');
+  const logo = document.getElementById("logo");
 
-  const magnifying = document.getElementById('mob-search')
-
-
+  const magnifying = document.getElementById("mob-search");
 
   let active = true;
-  console.log('ide gaaas')
+  console.log("ide gaaas");
 
-  srchMenu.addEventListener('click', () =>{
-    if(!active){
-      srchDiv.classList.add('d-none')
-      logo.classList.remove('d-none');
-      magnifying.classList.remove('d-none');
+  srchMenu.addEventListener("click", () => {
+    if (!active) {
+      srchDiv.classList.add("d-none");
+      logo.classList.remove("d-none");
+      magnifying.classList.remove("d-none");
 
-      return active = true;
+      return (active = true);
     }
-    if(active){
-      srchDiv.classList.remove('d-none')
-      logo.classList.add('d-none')
-      magnifying.classList.add('d-none');
+    if (active) {
+      srchDiv.classList.remove("d-none");
+      logo.classList.add("d-none");
+      magnifying.classList.add("d-none");
 
-
-      return active = false;
+      return (active = false);
     }
-  })
-}
+  });
+};
 
 //sakriva ili prikazuje sadrzaj kartice na hover
-function showCardContent(){
+function showCardContent() {
   //pristupam svim elementima koji imaju klasu .cat-desc, to su u ovom slucaju naslovi u kartici categories
-const categories=Array.from(document.querySelectorAll('.cat-desc'));
+  const categories = Array.from(document.querySelectorAll(".cat-desc"));
 
-//funkcija koja uklanja klasu d-none kada strelica udje u polje kartice
-const removeHidden = (e)=>{
-  //upitnik proverava da li objekat sadrzi kljuc pre nego sto nad njim izvrsi metodu
-  e.target.querySelector('.cat-hover')?.classList.remove('d-none')
+  //funkcija koja uklanja klasu d-none kada strelica udje u polje kartice
+  const removeHidden = (e) => {
+    //upitnik proverava da li objekat sadrzi kljuc pre nego sto nad njim izvrsi metodu
+    e.target.querySelector(".cat-hover")?.classList.remove("d-none");
+  };
+
+  //funkcija koja dodaje klasu d-none kada strelica izadje is polja kartice
+  const addHidden = (e) => {
+    e.target.querySelector(".cat-hover")?.classList.add("d-none");
+  };
+  //posto imam niz sastavljen od naslova koji se nalaze u karticama, kartici pristupam kao roditelju
+  //
+  categories.forEach((category) => {
+    category.parentElement.addEventListener("mouseover", removeHidden);
+    category.parentElement.addEventListener("mouseleave", addHidden);
+  });
 }
 
-//funkcija koja dodaje klasu d-none kada strelica izadje is polja kartice
-const addHidden=(e)=>{
-  e.target.querySelector('.cat-hover')?.classList.add('d-none')
-
-}
-//posto imam niz sastavljen od naslova koji se nalaze u karticama, kartici pristupam kao roditelju
-//
-categories.forEach(category=>{
-category.parentElement.addEventListener('mouseover',removeHidden)
-category.parentElement.addEventListener('mouseleave',addHidden)
-
-})
-}
-
-
-function getYear(){
-  const year = document.getElementById('year');
+function getYear() {
+  const year = document.getElementById("year");
   const currentYear = new Date().getFullYear();
   console.log(currentYear);
 
   year.innerText = currentYear;
 }
 
+// Validacija
+function validation(){
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const text = document.getElementById("questions");
+  const form = document.getElementById("form");
+  const errorElement = document.getElementById('error');
+  
+  form.addEventListener('submit', (e) => {
+    let messages = []
+    if (name.value === '' || name.value == null) {
+      messages.push("Name is required");
+    }
+  
+
+    if(email.value === '' || email.value == null){
+      messages.push ('Email is required');
+    }
+
+    if(text.value === '' || text.value == null){
+      messages.push('This filed is required');
+    }
+
+    if (messages.length > 0) {
+      e.preventDefault()
+      errorElement.innerText = messages.join(', ')
+    }
+  });
+  
+}
 
 
-window.addEventListener('DOMContentLoaded', (event) => {
+
+
+
+
+
+
+window.addEventListener("DOMContentLoaded", (event) => {
   //funkciju pozivamo tek kada se dom ucita, kako se ne bi desilo da se ona pozove a brauzer jos uvek nije formirao dom element
   phoneMenu();
   catMenu();
   searchMenu();
   showCardContent();
   getYear();
-})
+  validation();
+});
